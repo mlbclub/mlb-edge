816ef38d2761582cb2eeb37c5d18e25119f8bd14
mlbclub <leeminhoo960809@gmail.com>
2026-09-05 20:59:10 +0900

    Test explicit underdog discovery ranking

--- mlb_edge_public_web_designed/tests/test_ranked_picks.py
+++ mlb_edge_public_web_designed/tests/test_ranked_picks.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 import pandas as pd
-from mlb_model.recommend import select_betting_picks, choose_recommendation
+from mlb_model.recommend import select_betting_picks, select_underdog_picks, choose_recommendation
 from mlb_model.live import _prediction_assets, _needs_game_context
 
 
@@ -18,6 +18,29 @@ class RankedPickTests(unittest.TestCase):
         self.assertEqual([c['model_prob'] for g,c in picks], [.51+i*.005 for i in (7,6,5,4,3,2,1,0)])
         self.assertEqual(choose_recommendation([pairs[0][1]], {})['label'], 'BET')
         self.assertEqual(len(select_betting_picks(pairs[:2])), 1)
+
+    def test_underdog_board_catches_positive_value_dogs_without_probability_boost(self):
+        dog1 = dict(market='moneyline', rule_market='underdog_moneyline', model_prob=.47,
+                    market_prob=.39, odds=2.45, edge=.08, ev=.1515, pick='DOG A')
+        fav1 = dict(market='moneyline', rule_market='moneyline', model_prob=.53,
+                    market_prob=.61, odds=1.55, edge=-.08, ev=-.1785, pick='FAV A')
+        dog2 = dict(market='moneyline', rule_market='underdog_moneyline', model_prob=.44,
+                    market_prob=.36, odds=2.65, edge=.08, ev=.166, pick='DOG B')
+        weak_dog = dict(market='moneyline', rule_market='underdog_moneyline', model_prob=.39,
+                        market_prob=.34, odds=2.75, edge=.05, ev=.0725, pick='WEAK DOG')
+        pairs = [
+            ({'game_pk': 1}, fav1), ({'game_pk': 1}, dog1),
+            ({'game_pk': 2}, dog2), ({'game_pk': 3}, weak_dog),
+        ]
+        hit_board = select_betting_picks(pairs)
+        self.assertEqual(hit_board[0][1]['pick'], 'FAV A')
+        dogs = select_underdog_picks(pairs)
+        self.assertEqual([c['pick'] for _, c in dogs], ['DOG A', 'DOG B'])
+        self.assertEqual(dogs[0][1]['model_prob'], .47)
+        rec = choose_recommendation([fav1, dog1], {})
+        self.assertEqual(rec['pick'], 'FAV A')
+        self.assertEqual(rec['underdog_alternative']['pick'], 'DOG A')
+        self.assertEqual(rec['underdog_alternative']['model_prob'], .47)
 
     def test_missing_odds_are_not_fabricated(self):
         self.assertEqual(select_betting_picks([({'game_pk':1},dict(market='moneyline',model_prob=.8,odds=None))]), [])
